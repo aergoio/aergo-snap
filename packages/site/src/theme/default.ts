70627ba9d43b9cbef7d01fde3f26bb7c43ea60f8
findmytrueself <load1999@gmail.com>
2023-11-10 12:38:07 +0900

    Update Fonts theme & colors

--- packages/site/src/theme/default.ts
+++ packages/site/src/theme/default.ts
@@ -7,17 +7,38 @@ const breakpoints = ['600px', '768px', '992px'];
  */
 const theme = {
   fonts: {
-    default:
-      '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+    default: 'Outfit-Regular',
     code: 'ui-monospace,Menlo,Monaco,"Cascadia Mono","Segoe UI Mono","Roboto Mono","Oxygen Mono","Ubuntu Monospace","Source Code Pro","Fira Mono","Droid Sans Mono","Courier New", monospace',
   },
   fontSizes: {
     heading: '5.2rem',
     mobileHeading: '3.6rem',
+    h1: '3.2rem',
     title: '2.4rem',
     large: '2rem',
-    text: '1.6rem',
-    small: '1.4rem',
+    text: '1.8rem',
+    small: '1.6rem',
+    xsmall: '1.4rem',
+    xxsmall: '1.2rem',
+  },
+  lineHeights: {
+    h1: '5.6rem',
+  },
+  fontWeights: {
+    normal: 'normal',
+    bold: 'bold',
+    300: '300',
+    400: '400',
+    500: '500',
+    600: '600',
+    700: '700',
+  },
+
+  spacing: {
+    large: '1.6rem',
+    tiny1: '1rem',
+    tiny2: '0.8rem',
+    small: '0.6rem',
   },
   radii: {
     // default: '24px',
@@ -66,15 +87,15 @@ export const light: DefaultTheme = {
       default: '#6F4CFF',
       inverse: '#FFFFFF',
       // Blue
-      blue1: '#279ECC',
-      blue2: '#84CEEB',
-      blue3: '#ECF8FD',
+      1: '#279ECC',
+      2: '#84CEEB',
+      3: '#ECF8FD',
     },
     secondary: {
       // Pink
-      pink1: '#E4097D',
-      pink2: '#F894C9',
-      pink3: '#FFF1F9',
+      1: '#E4097D',
+      2: '#F894C9',
+      3: '#FFF1F9',
     },
     card: {
       default: '#FFFFFF',
@@ -85,6 +106,7 @@ export const light: DefaultTheme = {
       muted: '#d73a4919',
     },
     grey: {
+      white: '#FFFFFF',
       grey1: '#F0F0F0',
       grey2: '#D8D8D8',
       grey3: '#BABABA',
@@ -92,11 +114,18 @@ export const light: DefaultTheme = {
       grey6: '#686767',
       grey7: '#454344',
       grey8: '#231F20',
-      white: '#FFFFFF',
     },
     gradation: {
       gradation1:
+        'linear-gradient(#fff, #fff), linear-gradient(to right, #279ecc, #e4097d)',
+      gradation2:
+        'linear-gradient(#fff, #fff), linear-gradient(to right, #279ecc, #f894c8)',
+      gradation3:
+        'linear-gradient(#fff, #fff), linear-gradient(to right, #279ecc, #fff1f9)',
+      gradation4:
         'linear-gradient(#fff, #fff), linear-gradient(to right, #279ecc, #a13e99)',
+      gradation5:
+        'linear-gradient(#fff, #fff), linear-gradient(to right, #9a449c, #e30a7d)',
     },
   },
   ...theme,
@@ -124,11 +153,21 @@ export const dark: DefaultTheme = {
       inverse: '#24272A',
     },
     border: {
-      default: '#848C96',
+      default: '#24272A',
     },
     primary: {
       default: '#6F4CFF',
       inverse: '#FFFFFF',
+      // Blue
+      1: '#279ECC',
+      2: '#0F749B',
+      3: '#093140',
+    },
+    secondary: {
+      // Pink
+      1: '#F54A92',
+      2: '#972D5A',
+      3: '#3F1D2F',
     },
     card: {
       default: '#141618',
@@ -139,6 +178,7 @@ export const dark: DefaultTheme = {
       muted: '#d73a4919',
     },
     grey: {
+      white: '#FFFFFF',
       grey1: '#F0F0F0',
       grey2: '#D8D8D8',
       grey3: '#BABABA',
@@ -146,9 +186,19 @@ export const dark: DefaultTheme = {
       grey6: '#686767',
       grey7: '#454344',
       grey8: '#231F20',
-      white: '#FFFFFF',
     },
-    gradation: {},
+    gradation: {
+      gradation1:
+        'linear-gradient(#fff, #fff), linear-gradient(to right, #279ecc, #e4097d)',
+      gradation2:
+        'linear-gradient(#fff, #fff), linear-gradient(to right, #279ecc, #f894c8)',
+      gradation3:
+        'linear-gradient(#fff, #fff), linear-gradient(to right, #279ecc, #fff1f9)',
+      gradation4:
+        'linear-gradient(#fff, #fff), linear-gradient(to right, #279ecc, #a13e99)',
+      gradation5:
+        'linear-gradient(#fff, #fff), linear-gradient(to right, #9a449c, #e30a7d)',
+    },
   },
   ...theme,
 };
@@ -160,6 +210,13 @@ export const dark: DefaultTheme = {
  * @returns Global style React component.
  */
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Outfit-Regular';
+    src: url(../assets/fonts/Outfit-Regular.ttf);
+    font-weight: normal;
+    font-style: normal;
+  }
+
   html {
     /* 62.5% of the base size of 16px = 10px.*/
     font-size: 62.5%;
