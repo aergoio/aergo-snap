5919c22e05dc0c3da76f972a4b465c09042fd1aa
findmytrueself <load1999@gmail.com>
2023-11-10 12:40:15 +0900

    Update LoadingBackdrop font color

--- packages/site/src/components/ui/molecule/LoadingBackdrop/LoadingBackdrop.style.ts
+++ packages/site/src/components/ui/molecule/LoadingBackdrop/LoadingBackdrop.style.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  color: ${(props) => props.theme.colors.background.default};
+  color: ${(props) => props.theme.colors.primary[3]};
   font-size: ${(props) => props.theme.fontSizes.large};
   font-weight: 900;
-  /* line-height: 67.2px; */
   margin: 0;
   position: absolute;
   top: 50%;
@@ -12,8 +11,4 @@ export const Wrapper = styled.div`
   transform: translate(-50%, -50%);
   text-align: center;
   white-space: nowrap;
-  /* h3 {
-    font-weight: ${(props) => props.theme.fontSizes.text};
-    font-family: ${(props) => props.theme.fonts.default};
-  } */
 `;
