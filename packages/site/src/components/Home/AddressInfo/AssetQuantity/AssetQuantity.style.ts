ea75cfc32731d120495b57ed2c042a81ecc6d13b
findmytrueself <load1999@gmail.com>
2023-11-28 17:39:10 +0900

    Update Layout

--- packages/site/src/components/Home/AddressInfo/AssetQuantity/AssetQuantity.style.ts
+++ packages/site/src/components/Home/AddressInfo/AssetQuantity/AssetQuantity.style.ts
@@ -7,11 +7,12 @@ export const Wrapper = styled.div`
 `;
 export const Asset = styled.div`
   font-size: ${(props) => props.theme.fontSizes.h1};
-  line-height: ${(props) => props.theme.lineHeights.h1};
+  margin-top: ${(props) => props.theme.spacing.large};
   font-weight: 600;
 `;
 
 export const Dollor = styled.div`
+  margin-top: ${(props) => props.theme.spacing.small};
   color: ${(props) => props.theme.colors.grey.grey4};
   font-size: ${(props) => props.theme.fontSizes.large};
   font-weight: 500;
