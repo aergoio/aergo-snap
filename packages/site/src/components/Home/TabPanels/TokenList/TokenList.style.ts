6fef6dbb99dffde7b3b63dbb036e62d2509ab065
findmytrueself <load1999@gmail.com>
2023-11-28 17:44:54 +0900

    Add TokenList

--- packages/site/src/components/Home/TabPanels/TokenList/TokenList.style.ts
+++ packages/site/src/components/Home/TabPanels/TokenList/TokenList.style.ts
@@ -6,6 +6,7 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  padding: ${(props) => props.theme.spacing.large};
 `;
 
 export const ButtonWrapper = styled.div`
